2fff35081758e5e470b4f3b605bc4a4e32571467
lyuxc-unknow <2675796669@qq.com>
2022-07-09 19:33:59 +0800

    first push

---
+++ src/main/java/bathayypmod/BatHappyMod.java
@@ -0,0 +1,24 @@
+package bathayypmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("bathappymod_unofficial")
+public class BatHappyMod {
+    public static final Logger LOGGER = LogManager.getLogger("bathappymod_unofficial");
+
+    public static final String[] banModId = new String[] {
+            "torcherino", "xijun", "bacteria"
+    };
+
+    public BatHappyMod() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON,BatHappyModConfig.CommonConfig);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(BatHappyModConfig::init);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
---
+++ src/main/java/bathayypmod/BatHappyModConfig.java
@@ -0,0 +1,42 @@
+package bathayypmod;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BatHappyModConfig {
+    public static ForgeConfigSpec CommonConfig;
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> banMods;
+    private static final ForgeConfigSpec.ConfigValue<? extends String> crashTip;
+    private static final ForgeConfigSpec.BooleanValue showModid;
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        builder.comment("ban mods list").push("Blacklist");
+        banMods = builder.defineList("List", Arrays.asList(BatHappyMod.banModId), obj -> true);
+        crashTip = builder.define("tip","你的MC看起来崩溃了,是因为:");
+        showModid = builder.define("Whether to display the mod ID?",true);
+        builder.pop();
+        CommonConfig = builder.build();
+    }
+
+    public static void init(FMLCommonSetupEvent event) {
+        BatHappyMod.LOGGER.info("hello world");
+        if(BatHappyModConfig.banMods.get().toString().equalsIgnoreCase("bathappymod_unofficial") || banMods.get().isEmpty()) {
+            banMods.get().remove("bathappymod_unofficial");
+            banMods.set(Arrays.asList(BatHappyMod.banModId));
+            //throw new RuntimeException("Mod list is null");
+        }
+        for(String modIds : BatHappyModConfig.banMods.get()) {
+            if(ModList.get().isLoaded(modIds)) {
+                if(BatHappyModConfig.showModid.get())
+                    throw new RuntimeException(BatHappyModConfig.crashTip.get() + modIds);
+                else
+                    throw new RuntimeException(BatHappyModConfig.crashTip.get());
+            }
+        }
+    }
+}
